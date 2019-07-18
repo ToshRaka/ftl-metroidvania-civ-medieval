636e59620cb9ebe0e7dc9993990aa363a7c62f03
Gautier Berthou <gautier.berthou@outlook.fr>
2019-07-19 00:14:25 +0200

    Rectangle in all directions

--- src/World.gd
+++ src/World.gd
@@ -5,7 +5,21 @@ onready var characters := [$Character1, $Character2]
 onready var selector : ColorRect = $SelectCharacter
 
 var large_selection : bool = false
+var select_anchor : Vector2 = Vector2()
 var selected_characters = []
+
+func tweak_rectangle(begin : Vector2, end : Vector2):
+	if begin.x > end.x:
+		var temp := begin.x
+		begin.x = end.x
+		end.x = temp
+	
+	if begin.y > end.y:
+		var temp := begin.y
+		begin.y = end.y
+		end.y = temp
+	
+	return [begin, end]
 
 func _input(event : InputEvent) -> void :
 	if event is InputEventMouseButton:
@@ -17,7 +31,7 @@ func _input(event : InputEvent) -> void :
 						character.path = path
 						character.selected = false # Arbitrarily deselect character once they got a target
 			elif event.button_index == BUTTON_LEFT: # Select phase
-				var was_selected = false
+				var was_selected : bool = false
 				for character in characters:
 					character.selected = character.CollisionMouse.hover
 					was_selected = was_selected or character.selected
@@ -28,6 +42,7 @@ func _input(event : InputEvent) -> void :
 					selector.set_begin(event.global_position)
 					selector.set_end(event.global_position)
 					selector.visible = true
+					select_anchor = event.global_position
 		
 		else: # Mouse button released
 			large_selection = false
@@ -35,4 +50,8 @@ func _input(event : InputEvent) -> void :
 
 	elif event is InputEventMouseMotion:
 		if large_selection:
-			selector.set_end(event.global_position)
+			var temp = tweak_rectangle(select_anchor, event.global_position)
+			var begin : Vector2 = temp[0]
+			var end : Vector2 = temp[1]
+			selector.set_begin(begin)
+			selector.set_end(end)
